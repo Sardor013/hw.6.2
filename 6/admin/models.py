e199eb7e44070038234d2fe40c0360ac12c901b1
Sardor013 <sardoromonqulov8@gmail.com>
2024-05-12 20:39:16 +0500

    projects

--- 6/admin/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-class admink(models.Model):
-    user = models.CharField(max_length=225)
-    name = models.CharField(max_length=225)
-    job = models.CharField(max_length=225)
-
-    def __str__(self):
-        return self.name
-# Create your models here.
